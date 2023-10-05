416b4a7274491d0f2108983614647fbf29b89326
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-06 01:47:56 +0200

    still allocating

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,20 @@
 //#[global_allocator]
 //static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+use js_sys::Object;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+use web_sys::Window;
+use wasm_bindgen::JsCast;
 
 // wee_alloc_WeeAlloc_as_core_alloc_global_GlobalAlloc_alloc_hb4dfa88b790f3b3d
+
+#[wasm_bindgen]
+extern "C" {
+    type Global;
+
+    #[wasm_bindgen(getter, catch, static_method_of = Global, js_class = window, js_name = window)]
+    fn get_window() -> Result<Object, JsValue>;
+}
 
 #[wasm_bindgen(start)]
 fn run() {
@@ -13,7 +24,10 @@ fn run() {
 
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
-    let window = web_sys::window().unwrap();
+
+    // the comment in the source code even says this is large
+    //let window = Global::get_window().unwrap().dyn_into::<Window>().ok();
+    //let window = web_sys::window().unwrap();
     //let document = window.document().expect("should have a document on window");
     //let body = document.body().expect("document should have a body");
 
