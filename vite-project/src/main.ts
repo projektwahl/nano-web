97bda61e74a16eaef995fa998c65005dc8346a99
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-07 00:07:24 +0200

    Jo

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,7 +1,3 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 import wasmUrl from '../../target/wasm32-unknown-unknown/release/nano_web.wasm?url'
 
 let module = await WebAssembly.compileStreaming(fetch(wasmUrl))
@@ -18,25 +14,5 @@ let instance = await WebAssembly.instantiate(module, {
   },
 })
 
-console.log((instance.exports as any).add(1, 2)); // "3"
+console.log((instance.exports as any).add(1, 2));
 console.log((instance.exports as any).get_global_value())
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
