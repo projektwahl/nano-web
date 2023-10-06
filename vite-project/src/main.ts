ec0627e226559fde3c20c6c6536ea3338fcf46df
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-10-07 00:31:28 +0200

    Seems to work well

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,30 +1,35 @@
 import wasmUrl from '../../target/wasm32-unknown-unknown/release/nano_web.wasm?url'
 
 let module = await WebAssembly.compileStreaming(fetch(wasmUrl))
-let instance = await WebAssembly.instantiate(module, {
+let exports = (await WebAssembly.instantiate(module, {
   env: {
     console_log(arg: number) {
       console.log(arg)
     },
     peekaboo(arg: number) {
-      let table = instance.exports.__indirect_function_table as WebAssembly.Table
+      let table = exports.__indirect_function_table
       let f = table.get(arg)
       let result = f(1)
       console.log("result", result)
     },
     consoleLogString(offset: number, length: number) {
-      const bytes = new Uint8Array(instance.exports.memory as unknown as ArrayBufferLike, offset, length);
+      const bytes = new Uint8Array(exports.memory.buffer, offset, length);
       const string = new TextDecoder("utf8").decode(bytes);
       console.log(string);
     },
   },
-})
+})).exports as {
+  memory: WebAssembly.Memory,
+  __indirect_function_table: WebAssembly.Table,
+  GLOBALL: WebAssembly.Global,
+  test: () => void,
+  add: (a: number, b: number) => number,
+};
 
-let memory = instance.exports.memory as WebAssembly.Memory
-(instance.exports as any).test();
-let global = instance.exports.GLOBALL as WebAssembly.Global
+exports.test();
+let global = exports.GLOBALL
 console.log("memory offset", global.value)
-let global_mem = new Uint32Array(memory.buffer, global.value);
+let global_mem = new Uint32Array(exports.memory.buffer, global.value);
 console.log("value: ", global_mem.at(0));
-(instance.exports as any).add(1, 1)
+exports.add(1, 1)
 console.log("value: ", global_mem.at(0));
